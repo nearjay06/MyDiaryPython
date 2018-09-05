cc70c672937866f8cc9893f4f26679f19580a752
nearjay06 <nearjoan2018@gmail.com>
2018-09-05 14:07:07 +0300

    modify yob file

--- yob.py
+++ yob.py
@@ -3,14 +3,16 @@ from datetime import date
 
 
 def age():
- yob= int(input( 'Enter year of birth:  '))
- today_date = date.today().year
- age= today_date - yob
- if age < 18:
-    print('minor')
- elif age >= 18 and age <= 36:
-    print('youth')
- else:
-    print('elder')
+    yob= int(input( 'Enter year of birth:  '))                    
+    today_date = date.today().year
+    age= today_date - yob
+    if age < 18:
+        print('minor')
+    elif age >= 18 and age <= 36:
+        print('youth')    
+    else:
+        print('elder')    
 
-age()
+age()           
+
+
